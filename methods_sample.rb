58ea1339dcf9e123110cd38af7b59141bc402c41
DPL-3 <tylerdiamse@DPL-3s-iMac.local>
2014-02-10 11:43:56 -0800

    create a file named methods_sample.rb

---
+++ methods_sample.rb
@@ -0,0 +1,15 @@
+#http://apidock.com/ruby/String/split
+
+"now's the time".split
+
+
+
+#http://apidock.com/ruby/Array/join
+
+["now's", "the", "time"].join(" ")
+
+
+
+# A local variable is only visible/usable in the method in which it is defined.
+
+# An instance variable, on the other hand, is visible anywhere else in the instance of the class in which it has been defined
